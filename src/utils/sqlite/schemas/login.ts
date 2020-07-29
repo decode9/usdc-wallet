19a79b9280f19229c46e1704e3e522f1de6cec54
Jorge Bastidas <jorgebastidas9@gmail.com>
2020-07-28 23:32:56 -0400

    eliminate autoincrement

--- src/utils/sqlite/schemas/login.ts
+++ src/utils/sqlite/schemas/login.ts
@@ -1,7 +1,7 @@
 export default {
     table: 'user',
     colums: {
-        id: { type: 'INTEGER', primaryKey: true, nullable: false, autoIncrement: true, unique: true },
+        id: { type: 'INTEGER', primaryKey: true, nullable: false, unique: true },
         username: { type: 'STRING', nullable: false, unique: true },
         password: { type: 'STRING', nullable: false, unique: true }
     }
